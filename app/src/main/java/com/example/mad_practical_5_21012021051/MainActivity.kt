3d7241e114e0042e8bffbd19f3a5604abde857f1
student <student@gnu.ac.in>
2023-08-18 12:26:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad_practical_5_21012021051/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.mad_practical_5_21012021051
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val browseButton:Button=findViewById(R.id.Btn_Browse)
+        val browseButton2:Button=findViewById(R.id.Btn_Browse)
+        val browseButton:Button=findViewById(R.id.Btn_Browse)
+        val browseButton:Button=findViewById(R.id.Btn_Browse)
+        val browseButton:Button=findViewById(R.id.Btn_Browse)
+        val browseButton:Button=findViewById(R.id.Btn_Browse)
+
+    }
+
+}
